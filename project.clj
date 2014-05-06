cf96a3409cebb8386c76dde3ddd895d3e92503c9
Rory Gibson <rory.gibson@brightnorth.co.uk>
2014-05-06 09:44:39 +0100

    Logging config and project.clj additions

--- project.clj
+++ project.clj
@@ -1,10 +1,22 @@
 (defproject linkin "0.1.0-SNAPSHOT"
   :description "Web crawling with async magic"
+  
   :url "http://github.com/rorygibson/linkin"
+  
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
+  
+  :main linkin.core
+
+  :dependencies [[org.clojure/clojure "1.6.0"]
                  [http-kit "2.1.18"]
                  [org.jsoup/jsoup "1.7.3"]
-                 [log4j "1.2.17" :exclusions [javax.mail/mail javax.jms/jms com.sun.jdmk/jmxtools com.sun.jmx/jmxri]]
-                 [org.clojure/tools.logging "0.2.6"]])
+                 [log4j "1.2.17"]
+                 [org.clojure/tools.logging "0.2.6"]]
+
+  :profiles { :dev
+              {
+               :dependencies [[midje "1.6.3"]]
+               :plugins      [[lein-ancient "0.5.4" :exclusions [org.clojure/clojure commons-codec org.clojure/data.xml]]
+                              [lein-midje "3.1.1"]]}
+              })

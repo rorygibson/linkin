573f500cef3988ecb39b3b08cc031b5e6996f380
Rory Gibson <rory.gibson@brightnorth.co.uk>
2014-05-06 08:34:38 +0100

    Wrap all state in a single resettable var

--- src/linkin/core.clj
+++ src/linkin/core.clj
@@ -5,12 +5,48 @@
            [org.jsoup.nodes Document]))
 
 
-(def crawled-urls (atom #{}))
-(def link-agent (agent '()))
-(def handler-agent (agent ""))
-(def base-url (atom ""))
+;;
+;; Constants
+;; 
+
+(def TEXT-HTML "text/html")
 
 
+;;
+;; State
+;;
+
+(defn initial-state
+  []
+  { :crawled-urls #{}
+    :link-agent (agent "")
+    :handler-agent (agent "")
+    :base-url ""})
+
+
+(def application (atom (initial-state)))
+(defn reset-application! [] (swap! application initial-state))
+
+(defn crawled-urls [] (:crawled-urls @application))
+(defn link-agent [] (:link-agent @application))
+(defn handler-agent [] (:handler-agent @application))
+(defn base-url [] (:base-url @application))
+
+
+(defn mark-as-crawled
+  [url]
+  (swap! application (fn [app] (assoc app :crawled-urls (conj (:crawled-urls app) url)))))
+
+
+(defn set-base-url
+  [u]
+  (swap! application assoc :base-url u))
+
+
+
+
+
+         
 (defn local?
   [url base-url]
   (.startsWith url base-url))
@@ -34,14 +70,14 @@
 
 (defn simple-body-parser
   [url content-type body]
-  (if (= content-type "text/html")
+  (if (= content-type TEXT-HTML)
     (debug "[simple-body-parser] got [" url "] of type [" content-type "] with " (count body) " bytes data")))
 
 
 (defn extract-anchors
   "Uses JSoup magic to extract anchor tags"
   [body ^String content-type ^String base-uri]
-  (if (= content-type "text/html")
+  (if (= content-type TEXT-HTML)
     (let [body (if (nil? body) "" body)
           doc (Jsoup/parse body)
           doc (doto doc (.setBaseUri base-uri))
@@ -50,17 +86,12 @@
     []))
 
 
-(defn mark-as-crawled
-  [url]
-  (swap! crawled-urls conj url))
-
-
 (defn response-handler
   "Called as a callback by the HTTP-Kit fetcher.
    Obtains and enqueues for processing the anchors and full body text from the response"
   [body-parser url {:keys [status headers body error opts] :as resp}]
 
-  (if (crawl? url @crawled-urls @base-url)
+  (if (crawl? url (crawled-urls) (base-url))
 
     (let [content-type (:content-type headers)
           anchors (extract-anchors body content-type url)]
@@ -68,17 +99,17 @@
       (trace "[response-handler] got" url content-type)
         
       (doseq [link anchors]      
-        (if (crawl? link @crawled-urls @base-url)
-          (send link-agent (fn [_] (http/get link (partial response-handler body-parser link))))))
+        (if (crawl? link (crawled-urls) (base-url))
+          (send (link-agent) (fn [_] (http/get link (partial response-handler body-parser link))))))
 
-      (send handler-agent (fn [_] (body-parser url content-type body))))))
+      (send (handler-agent) (fn [_] (body-parser url content-type body))))))
 
 
 (defn crawl
   "Start crawling at the given URL"
   [url body-parser]
-  (swap! base-url (fn [_] url))
+  (set-base-url url)
   
-  (info "[crawl] Starting crawl of" @base-url)
+  (info "[crawl] Starting crawl of" (base-url))
 
-  (http/get url {} (partial response-handler body-parser @base-url)))
+  (http/get url {} (partial response-handler body-parser (base-url))))

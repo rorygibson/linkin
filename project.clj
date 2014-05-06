b331f8604b4d57acd6eb4dc200844dee6462d29a
Rory Gibson <rory.gibson@brightnorth.co.uk>
2014-05-06 08:03:21 +0100

    Upgrade http-kit to latest version

--- project.clj
+++ project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
   :dependencies [[org.clojure/clojure "1.5.1"]
-                 [http-kit "2.1.16"]
+                 [http-kit "2.1.18"]
                  [org.jsoup/jsoup "1.7.3"]
                  [log4j "1.2.17" :exclusions [javax.mail/mail javax.jms/jms com.sun.jdmk/jmxtools com.sun.jmx/jmxri]]
                  [org.clojure/tools.logging "0.2.6"]])

c6d50cc0b7960d8cde8b5f228f591c462cb567d4
Rory Gibson <rory.gibson@brightnorth.co.uk>
2014-05-06 08:00:39 +0100

    Fix potential infinite loop, detect similar URLs

--- src/linkin/core.clj
+++ src/linkin/core.clj
@@ -1,51 +1,75 @@
 (ns linkin.core
-  (:require [org.httpkit.client :as http])
+  (:require [org.httpkit.client :as http]
+            [clojure.tools.logging :refer [debug info error warn trace]])
   (:import [org.jsoup Jsoup]
            [org.jsoup.nodes Document]))
 
 
-(def links (atom '()))
-
-
+(def crawled-urls (atom #{}))
+(def link-agent (agent '()))
 (def handler-agent (agent ""))
+(def base-url (atom ""))
 
 
+(defn crawl?
+  [url]
 
-(defn enqueue-links
-  [ls]
-  (println "[enqueue-links] accepting links" ls)
-  (swap! links into ls))
+  (let [similar-url (str url "#")
+        done (some #{url} @crawled-urls)
+        done-similar (some #{similar-url} @crawled-urls)
+        not-done (not (or done done-similar))
+        local (.startsWith url @base-url)
+        result (and not-done local)]
+    result))
 
 
-(defn run-user-handler
-  [handler-fn url h]
-  (println "[run-user-handler] sending to" (.toString handler-fn))
-  (send handler-agent (fn [_] (handler-fn url h))))
+(defn simple-body-parser
+  [url content-type body]
+  (if (= content-type "text/html")
+    (debug "[simple-body-parser] got [" url "] of type [" content-type "] with " (count body) " bytes data")))
 
 
 (defn extract-anchors
   "Uses JSoup magic to extract anchor tags"
-  [^String body ^String base-uri]
-  (let [doc (Jsoup/parse body)
-        doc (doto doc (.setBaseUri base-uri))
-        anchors (.select doc "a")]
-    (map #(.attr % "abs:href") anchors)))
+  [body ^String content-type ^String base-uri]
+  (if (= content-type "text/html")
+    (let [body (if (nil? body) "" body)
+          doc (Jsoup/parse body)
+          doc (doto doc (.setBaseUri base-uri))
+          anchors (.select doc "a")]
+      (map #(.attr % "abs:href") anchors))
+    []))
 
 
-(defn http-kit-handler
+(defn mark-as-crawled
+  [url]
+  (swap! crawled-urls conj url))
+
+
+(defn response-handler
   "Called as a callback by the HTTP-Kit fetcher.
    Obtains and enqueues for processing the anchors and full body text from the response"
-  [handler url {:keys [status headers body error opts] :as resp}]
-  (println "[http-kit-handler] called for" url)
-  
-  (if error
-    (println "[http-kit-handler] Failed, exception is " error)    
-    (do
-      (enqueue-links (extract-links body url))
-      (run-user-handler handler url body))))
+  [body-parser url {:keys [status headers body error opts] :as resp}]
+
+  (if (crawl? url)
+
+    (let [content-type (:content-type headers)
+          anchors (extract-anchors body content-type url)]
+      (mark-as-crawled url)
+      (trace "[response-handler] got" url content-type)
+        
+      (doseq [link anchors]      
+        (if (crawl? link)
+          (send link-agent (fn [_] (http/get link (partial response-handler body-parser link))))))
+
+      (send handler-agent (fn [_] (body-parser url content-type body))))))
 
 
 (defn crawl
   "Start crawling at the given URL"
-  [url handler]
-  (http/get url {} (partial http-kit-handler handler url)))
+  [url body-parser]
+  (swap! base-url (fn [_] url))
+  
+  (info "[crawl] Starting crawl of" @base-url)
+
+  (http/get url {} (partial response-handler body-parser @base-url)))

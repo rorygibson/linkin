658c6a4a988b61a2ba494591cb90e4fc47073eef
Rory Gibson <rory.gibson@brightnorth.co.uk>
2014-05-06 08:02:31 +0100

    Don't need a gig of memory by default

--- project.clj
+++ project.clj
@@ -3,7 +3,6 @@
   :url "http://github.com/rorygibson/linkin"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :jvm-opts ["-Xmx1g" "-server"] 
   :dependencies [[org.clojure/clojure "1.5.1"]
                  [http-kit "2.1.16"]
                  [org.jsoup/jsoup "1.7.3"]
